fe4604728355827f1e543eb956ddcfe0319ee50a
ockonal <ockonal@wincode.wincode>
2010-06-05 16:33:24 +0300

    Game application is singlton-style now

---
+++ include/Engine.hpp
@@ -0,0 +1,14 @@
+#ifndef ENGINE_HPP
+#define	ENGINE_HPP
+
+#include "PseudoformCore.hpp"
+
+class Engine
+{
+    public:
+        //GameApplication &Application() const { return *GameApplication; }
+
+};
+
+#endif	/* ENGINE_HPP */
+
