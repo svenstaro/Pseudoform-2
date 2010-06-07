735309e4d185dff9f523ff45c540ba2d07d93e9c
ockonal <ockonal@wincode.wincode>
2010-06-07 14:44:26 +0300

    Upgrading of config manager. Now there is loading from own config file (storing pointers for all given configs). Also there is a new future - loading data list.

--- include/Managers/ConfigManager.hpp
+++ include/Managers/ConfigManager.hpp
@@ -4,27 +4,72 @@
 #include <boost/property_tree/ptree.hpp>
 #include <boost/property_tree/info_parser.hpp>
 
+#include <vector>
+#include <map>
+
 using boost::property_tree::ptree;
 using namespace std;
 
 class ConfigManager
 {
 	protected:
-		string mConfigPath;
-		ptree mPropertyTree;
+		string mDefaultPath;
+                map<string, ptree> mPtreeList;
+
+                void _forcePtree(const string &configPath)
+                {
+                    if (mPtreeList.count(configPath) == 0)
+                    {
+                        mPtreeList.insert(make_pair(configPath, ptree()));
+                    }
+                }
 
 	public:
-		ConfigManager(const string &filePath = "settings.info") :
-			mConfigPath(filePath)
+		ConfigManager(const string &configPath = "settings.info")
 		{
-			read_info(mConfigPath, mPropertyTree);
+                    mDefaultPath = configPath;
+                    _forcePtree(configPath);
+                    read_info(configPath, mPtreeList[configPath]);
 		}
 
 		template<typename T>
-		T getValue(const string &key, const T &defaultValue = T())
+		T getValue(const string &key, const T &defaultValue = T(), const string &ownConfig = "")
 		{
-			return mPropertyTree.get<T>(key, defaultValue);
+                    if (ownConfig != "")
+                    {
+                        _forcePtree(ownConfig);
+                        return mPtreeList[ownConfig].get<T>(key, defaultValue);
+                    }
+                    else
+                    {
+                        return mPtreeList[mDefaultPath].get<T>(key, defaultValue);
+                    }
 		}
+
+                vector<string> getListValue(const string &key, const string &ownConfig = "")
+                {
+                    vector<string> listValues;
+                    ptree workTree;
+
+                    if (ownConfig != "")
+                    {
+                        _forcePtree(ownConfig);
+                        workTree = mPtreeList[ownConfig];
+                    }
+                    else
+                    {
+                        workTree = mPtreeList[mDefaultPath];
+                    }
+
+                    bforeach(ptree::value_type &value, workTree.get_child(key))
+                    {
+                        cout << "!!!!!!!!!!!!!!!!!!!!!!!!!";
+                        cout << value.second.data();
+                        listValues.push_back(value.second.data());
+                    }
+
+                    return listValues;
+                }
 };
 
 #endif
