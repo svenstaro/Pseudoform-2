485c23f2879b76b0addc5c99888087e918dc94f6
Max Tyslenko <Ockonal@gmail.com>
2010-06-14 14:43:29 +0300

    Resource manager is now restructured single singleton

--- include/Managers/ResourcesManager.hpp
+++ include/Managers/ResourcesManager.hpp
@@ -1,7 +1,13 @@
 #ifndef RESOURCESMANAGER_HPP
 #define	RESOURCESMANAGER_HPP
 
-class ResourcesManager
+#include "Managers/LogManager.hpp"
+#include "Core/Utils.hpp"
+
+#include <boost/serialization/singleton.hpp>
+using namespace boost::serialization;
+
+class ResourceManager : public singleton<ResourceManager>
 {
     public:
         void addResourceLocationRecursive(Ogre::String path, Ogre::String group = "General")
@@ -30,15 +36,29 @@ class ResourcesManager
             if (rPtr.isNull()) return
 
             rPtr->unload();
+            if (rPtr->isLoaded())
+                LOG(FORMAT("The resource '%1%' wasn`t unloaded!", resourceName));
+            else
+                LOG(FORMAT("The resource '%1%' was unloaded!", resourceName));
+
             resMgr->remove(resourceName);
+            rPtr = resMgr->getByName(resourceName);
+            if (!rPtr.isNull())
+                LOG(FORMAT("The resource '%1%' wasn`t removed!", resourceName));
+            else
+                LOG(FORMAT("The resource '%1%' was removed!", resourceName));
         }
         
         void ReloadResource(Ogre::ResourceManager* resMgr, const std::string& resourceName, const std::string& resourceGroup)
         {
             Ogre::ResourcePtr rPtr = resMgr->load(resourceName, resourceGroup);
-            if (rPtr.isNull()) return;
+            if (rPtr.isNull()) LOG(FORMAT("The resource '%1%' no longer exists!", resourceName));
 
             rPtr->reload();
+            if (rPtr->isLoaded())
+                LOG(FORMAT("The resource '%1%' wasn`t unloaded!", resourceName));
+            else
+                LOG(FORMAT("The resource '%1%' was unloaded!", resourceName));
         }
 };
 
