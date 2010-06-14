86b8e8ae3440abdaecd1f179c984fb7738b716ce
Max Tyslenko <Ockonal@gmail.com>
2010-06-14 12:56:16 +0300

    Config manager is now restructured single singleton with his own define

--- include/Managers/ConfigManager.hpp
+++ include/Managers/ConfigManager.hpp
@@ -3,16 +3,20 @@
 
 #include <boost/property_tree/ptree.hpp>
 #include <boost/property_tree/info_parser.hpp>
-
+#include <boost/serialization/singleton.hpp>
 #include <boost/foreach.hpp>
+
+#include "Managers/LogManager.hpp"
 
 #include <vector>
 #include <map>
 
+using namespace boost::serialization;
 using boost::property_tree::ptree;
 using namespace std;
 
-class ConfigManager
+#define CONFIG(key, type, def) ConfigManager::get_mutable_instance().getValue<type>(key, def)
+class ConfigManager : public singleton<ConfigManager>
 {
     protected:
         string mDefaultPath;
@@ -27,16 +31,17 @@ class ConfigManager
         }
 
     public:
-        ConfigManager(const string &configPath = "settings.info")
+        ConfigManager()
         {
-            mDefaultPath = configPath;
-            _forcePtree(configPath);
-            read_info(configPath, mPtreeList[configPath]);
+            mDefaultPath = "settings.info";
+            _forcePtree(mDefaultPath);
+            read_info(mDefaultPath, mPtreeList[mDefaultPath]);
         }
 
         template<typename T>
         T getValue(const string &key, const T &defaultValue = T(), const string &ownConfig = "")
         {
+            if (key == "") LOG("Trying to get config value of empty key!");
             if (ownConfig != "")
             {
                 _forcePtree(ownConfig);
@@ -53,6 +58,7 @@ class ConfigManager
             vector<string> listValues;
             ptree workTree;
 
+            if (key == "") LOG("Trying to get config value of empty key!");
             if (ownConfig != "")
             {
                 _forcePtree(ownConfig);
