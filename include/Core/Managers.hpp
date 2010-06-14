342619051579eb73499cf209e916be42ed7306f2
Max Tyslenko <Ockonal@gmail.com>
2010-06-14 14:45:40 +0300

    We needn`t in one include for all managers now

--- include/Core/Managers.hpp
+++
@@ -1,9 +0,0 @@
-#ifndef MANAGERS_HPP
-#define	MANAGERS_HPP
-
-#include "Managers/ConfigManager.hpp"
-#include "Managers/LogManager.hpp"
-#include "Managers/ResourcesManager.hpp"
-
-#endif	/* MANAGERS_HPP */
-
