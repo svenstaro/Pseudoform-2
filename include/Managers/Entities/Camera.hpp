2b2b45ad87760a3c0048bfb15f18929918393ecc
Max Tyslenko <Ockonal@gmail.com>
2010-07-23 15:23:52 +0300

    Upgraded camera entity. Now predefined camera-style available

--- include/Managers/Entities/Camera.hpp
+++ include/Managers/Entities/Camera.hpp
@@ -2,7 +2,7 @@
 #define	CAMERA_HPP
 
 #include "Managers/EventManager.hpp"
-#include "Systems/GraphicSystem.hpp"
+#include "Core/Systems.hpp"
 #include "Core/Types.hpp"
 
 #include "Managers/Entities/Entity.hpp"
@@ -15,12 +15,34 @@ class Camera : public Entity
 		Ogre::Camera *mCamera;
 		Ogre::SceneNode *mPitchNode;
 
+		float mMove;
+		float mRotate;
+
     public:
+        enum CameraType
+        {
+        	FREE,
+        	ATTACHED,
+        	FIRST_PERSON,
+        	DONT_USE	// If we don't want predefined stuff
+        } mCameraType;
+
         Camera(const string cameraName);
         void configure(const float nearClip = 1, const float farClip = 1000, bool autoAR = true, const float FOV = 90,
         		vec3 pos = vec3(0, 1000, 1000), vec3 lookAt = vec3(0, 0, 0));
         Ogre::Camera *cameraHandle();
         Ogre::SceneNode *getPitchNode();
+        void setCameraType(CameraType type);
+
+        void setMoveStep(float step);
+        void setRotateStep(float step);
+        float getMoveStep();
+        float getRotateStep();
+
+		void onInited();
+		void onUpdated();
+		void onKeyPressed(sf::Event::KeyEvent &eventData);
+		void onMouseMoved(sf::Event::MouseMoveEvent &eventData);
 
         void _loadData();
         void update(float elapsed);
--- src/Managers/Entities/Camera.cpp
+++ src/Managers/Entities/Camera.cpp
@@ -7,8 +7,12 @@ Camera::Camera(const string cameraName)
     mEntityName = cameraName;
     mCamera = sceneMgr->createCamera(cameraName);
 
-    mNode = sceneMgr->getRootSceneNode()->createChildSceneNode("Node:Camera" + cameraName);
-    mNode->createChildSceneNode("Node:CameraPitch" + cameraName)->attachObject(mCamera);
+    mMove = 250;
+    mRotate = 0.13;
+
+    mNode = sceneMgr->getRootSceneNode()->createChildSceneNode("Node:Camera_" + cameraName);
+    mPitchNode = mNode->createChildSceneNode("Node:CameraPitch_" + cameraName);
+    mPitchNode->attachObject(mCamera);
 }
 
 void Camera::configure(const float nearClip, const float farClip, bool autoAR, const float FOV,
@@ -22,5 +26,131 @@ void Camera::configure(const float nearClip, const float farClip, bool autoAR, c
 
 Ogre::Camera *Camera::cameraHandle() { return mCamera; }
 Ogre::SceneNode *Camera::getPitchNode() { return mPitchNode; }
+
+float Camera::getMoveStep() { return mMove; }
+float Camera::getRotateStep() { return mRotate; }
+void Camera::setMoveStep(float step) { mMove = step; }
+void Camera::setRotateStep(float step) { mRotate = step; }
+
 void Camera::_loadData() { }
 void Camera::update(float elapsed) { }
+
+#define WIDTH GraphicSystem::get_mutable_instance().getWindow()->getWidth()
+#define HEIGHT GraphicSystem::get_mutable_instance().getWindow()->getHeight()
+
+void Camera::setCameraType(CameraType type)
+{
+	mCameraType = type;
+
+	if (type != Camera::DONT_USE)
+	{
+		CONNECT(Engine::Events::KeyEvent, "KeyPressed", &Camera::onKeyPressed)
+	    CONNECT(Engine::Events::MouseMoveEvent, "MouseMoved", &Camera::onMouseMoved);
+	    CONNECT0(Engine::Events::GlobalUpdateEvent, "Updated", &Camera::onUpdated);
+	    CONNECT0(Engine::Events::GlobalInitEvent, "Inited", &Camera::onInited);
+	}
+}
+
+void Camera::onInited()
+{
+	switch(mCameraType)
+	{
+		case Camera::FREE:
+			InputSystem::get_mutable_instance().Window().SetCursorPosition(WIDTH/2, HEIGHT/2);
+			cout << "On inited, cursor position set: " << WIDTH/2 << "; " << HEIGHT/2 << "\n";
+			InputSystem::get_mutable_instance().Window().ShowMouseCursor(false);
+
+		break;
+		case Camera::ATTACHED:
+			// ...
+
+		break;
+		case Camera::FIRST_PERSON:
+			// ...
+
+		break;
+	}
+}
+
+void Camera::onUpdated()
+{
+	switch(mCameraType)
+	{
+		case Camera::FREE:
+			InputSystem::get_mutable_instance().Window().SetCursorPosition(WIDTH/2, HEIGHT/2);
+
+		break;
+		case Camera::ATTACHED:
+			// ...
+
+		break;
+		case Camera::FIRST_PERSON:
+			// ...
+
+		break;
+	}
+}
+
+void Camera::onKeyPressed(sf::Event::KeyEvent &eventData)
+{
+	using namespace sf;
+	vec3 translation = vec3::ZERO;
+
+	switch(mCameraType)
+	{
+		case Camera::FREE:
+			if (eventData.Code == Key::W)
+			    translation.z -= mMove;
+			else if (eventData.Code == Key::S)
+			    translation.z += mMove;
+			else if (eventData.Code == Key::A)
+			    translation.x -= mMove;
+			else if (eventData.Code == Key::D)
+			    translation.x += mMove;
+			else if (eventData.Code == Key::Q)
+				translation.y += mMove;
+			else if (eventData.Code == Key::E)
+				translation -= mMove;
+
+			mNode->translate(mNode->getOrientation() * translation * GameApplication::get_const_instance().getElapsed());
+
+		break;
+		case Camera::ATTACHED:
+			// ...
+
+		break;
+		case Camera::FIRST_PERSON:
+			// ...
+
+		break;
+	}
+}
+
+void Camera::onMouseMoved(sf::Event::MouseMoveEvent &eventData)
+{
+	int offsetX = 0;
+	int offsetY = 0;
+
+	int centerX = WIDTH/2;
+	int centerY = HEIGHT/2;
+
+	switch(mCameraType)
+	{
+		case Camera::FREE:
+			offsetX = eventData.X - centerX;
+			offsetY = eventData.Y - centerY;
+
+		    mNode->yaw(deg(mRotate * -offsetX));
+		    mPitchNode->pitch(deg(mRotate * -offsetY));
+
+		break;
+		case Camera::ATTACHED:
+			// ...
+
+		break;
+		case Camera::FIRST_PERSON:
+			// ...
+
+		break;
+	}
+}
