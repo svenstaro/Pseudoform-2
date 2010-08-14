f436a7b7a66cc4050d10943a19a3f33aa7a121ab
Max Tyslenko <Ockonal@gmail.com>
2010-08-14 20:19:47 +0300

    Removed resource adding directly from source (now from config file)

--- samples/pong-cpp/main.cpp
+++ samples/pong-cpp/main.cpp
@@ -3,10 +3,10 @@
 using namespace std;
 using namespace Engine;
 
-int main() {
+int main()
+{
     Managers::GetLog().write("Adding new source location...", "pong.log");
-    Managers::GetResource().addResourceLocation("pong-media", true);
-
+    
     World::MakeCamera("MainCamera", true, Camera::FREE);
     World::MakeLight("MainLight", Ogre::Light::LT_DIRECTIONAL)->lightHandle()->setDirection( vec3(0, -1, 1) );
     World::MakeObject("background")->setScale(vec3(100, 100, 100));
