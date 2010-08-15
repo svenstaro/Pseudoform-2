fba5dc3a32767018d5dad35984b90952addd6a54
Max Tyslenko <Ockonal@gmail.com>
2010-08-15 10:04:02 +0300

    Added bindings of input system for the gui system.

--- src/Systems/InputSystem.cpp
+++ src/Systems/InputSystem.cpp
@@ -36,28 +36,44 @@ void InputSystem::update(float elapsed)
         using namespace Engine::Events;
         sf::Event::EventType type = localEvent.Type;
         
+        // Key events -----------------------------------------------------------------------------
         if (localEvent.Type == sf::Event::KeyPressed) {
             SIGNAL(KeyEvent, "KeyPressed", localEvent.Key);
+            GuiSystem::get_mutable_instance().getGui()->injectKeyPress(MyGUI::KeyCode::Enum(localEvent.Key.Code), 0);
+
         } else if (type == sf::Event::KeyReleased) {
             SIGNAL(KeyEvent, "KeyReleased", localEvent.Key);
-        } else if (type == sf::Event::Resized) {
-            SIGNAL(SizeEvent, "Resized", localEvent.Size);
-        } else if (type == sf::Event::TextEntered) {
-            SIGNAL(TextEvent, "TextEntered", localEvent.Text);
+            GuiSystem::get_mutable_instance().getGui()->injectKeyRelease(MyGUI::KeyCode::Enum(localEvent.Key.Code));
+
+        // Mouse events --------------------------------------------------------------------------
         } else if (type == sf::Event::MouseButtonPressed) {
             SIGNAL(MouseButtonEvent, "MouseButtonPressed", localEvent.MouseButton);
+            GuiSystem::get_mutable_instance().getGui()->injectMousePress(localEvent.MouseButton.X, localEvent.MouseButton.Y, MyGUI::MouseButton::Enum(localEvent.MouseButton.Button));
+
         } else if (type == sf::Event::MouseButtonReleased) {
             SIGNAL(MouseButtonEvent, "MouseButtonReleased", localEvent.MouseButton);
+            GuiSystem::get_mutable_instance().getGui()->injectMouseRelease(localEvent.MouseButton.X, localEvent.MouseButton.Y, MyGUI::MouseButton::Enum(localEvent.MouseButton.Button));
+
         } else if (type == sf::Event::MouseMoved) {
             SIGNAL(MouseMoveEvent, "MouseMoved", localEvent.MouseMove);
+            GuiSystem::get_mutable_instance().getGui()->injectMouseMove(localEvent.MouseMove.X, localEvent.MouseMove.Y, 0);
+
         } else if (type == sf::Event::MouseWheelMoved) {
             SIGNAL(MouseWheelEvent, "MouseWheelMoved", localEvent.MouseWheel);
+
+        // Joy events ----------------------------------------------------------------------------
         } else if (type == sf::Event::JoyButtonPressed) {
             SIGNAL(JoyButtonEvent, "JoyButtonPressed", localEvent.JoyButton);
         } else if (type == sf::Event::JoyButtonReleased) {
             SIGNAL(JoyButtonEvent, "JoyButtonReleased", localEvent.JoyButton);
         } else if (type == sf::Event::JoyMoved) {
             SIGNAL(JoyMoveEvent, "JoyMoved", localEvent.JoyMove);
+
+        // Window events -------------------------------------------------------------------------
+        } else if (type == sf::Event::Resized) {
+            SIGNAL(SizeEvent, "Resized", localEvent.Size);
+        } else if (type == sf::Event::TextEntered) {
+            SIGNAL(TextEvent, "TextEntered", localEvent.Text);
         } else if (type == sf::Event::Closed) {
             SIGNAL(CloseEvent, "WindowClosed", );
         } else if (type == sf::Event::LostFocus) {
