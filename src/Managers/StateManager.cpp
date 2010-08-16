a70240131a85c4542c90d3855ca4a5aa54f524d9
Max Tyslenko <Ockonal@gmail.com>
2010-08-16 16:18:16 +0300

    Some fail-checking in state manager

--- src/Managers/StateManager.cpp
+++ src/Managers/StateManager.cpp
@@ -1,4 +1,5 @@
 #include "Managers/StateManager.hpp"
+#include "Managers/LogManager.hpp"
 
 void StateManager::push(const StatePtr &state)
 {
@@ -15,11 +16,14 @@ StateManager::~StateManager()
 
 State *StateManager::pop()
 {
+	if (mStates.empty()) return StatePtr().get();
+
 	mActiveState = mStates.back();	// Store pointer for next state to use
 	mStates.pop_back();				// And remove it from list
 }
 
 State *StateManager::getActiveState()
 {
+	if (mActiveState.get() == NULL) LOG("[State Manager → getActiveState] There aren't any active state. Maybe, you didn't push one.");
 	return mActiveState.get();
 }
