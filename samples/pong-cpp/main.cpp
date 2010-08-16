39db2a21fdb4da6f5620f863aad9f67b598df709
Max Tyslenko <Ockonal@gmail.com>
2010-08-16 19:16:26 +0300

    New sample of states usage

--- samples/pong-cpp/main.cpp
+++ samples/pong-cpp/main.cpp
@@ -3,15 +3,34 @@
 using namespace std;
 using namespace Engine;
 
+class MenuState : public State
+{
+    public:
+        MenuState() {
+            Systems::GetGui().loadLayout("Pseudoform.layout");
+        }
+
+        void update() {}
+        void shutdown() {}
+};
+
+class GameState : public State
+{
+    public:
+        GameState() {
+            World::MakeCamera("MenuCamera", true, Camera::DONT_USE);
+            World::MakeLight("MainLight", Ogre::Light::LT_DIRECTIONAL)->lightHandle()->setDirection( vec3(0, -1, 1) );
+            World::MakeObject("background")->setScale(vec3(100, 100, 100));
+        }
+
+        void update() {}
+        void shutdown() {}
+};
+
 int main()
 {
-    Managers::GetLog().write("Adding new source location...", "pong.log");
-    
-    World::MakeCamera("MenuCamera", true, Camera::DONT_USE);
-    World::MakeLight("MainLight", Ogre::Light::LT_DIRECTIONAL)->lightHandle()->setDirection( vec3(0, -1, 1) );
-    World::MakeObject("background")->setScale(vec3(100, 100, 100));
-
-    Systems::GetGui().loadLayout("Pseudoform.layout");
+    Managers::GetState().push(new(MenuState));
+    Managers::GetState().push(new(GameState));
 
     World::Application().Start();
 
